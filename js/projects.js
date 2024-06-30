8a478b1996ae53c941df5c9acff04724ace60915
Quinten Kamphuis <163667554+quinten-kamphuis@users.noreply.github.com>
2024-06-30 15:12:49 +0200

    Add todo functions to projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,74 +1,156 @@
 import logger from './logger'
+import validation from './validation';
 
 const projects = []
 
-const generateId = () => {
+const generateProjectId = () => {
     return projects.length > 0 ? Math.max(...projects.map(p => p.id)) + 1 : 1;
 };
 
-const findProject = (id) => {
-    const index = projects.findIndex(project => project.id === id)
+const generateTodoId = (projectId) => {
+    const todos = findProject(projectId).todos
+    return todos.length > 0 ? Math.max(...todos.map(p => p.id)) + 1 : 1;
+};
+
+const findProject = (projectId) => {
+    const project = projects.find(project => project.id === projectId)
+    if (project) {
+        return project
+    }
+    logger.error('The project could not be found')
+    return null
+}
+
+const findProjectIndex = (projectId) => {
+    const index = projects.findIndex(project => project.id === projectId)
     if (index !== -1) {
         return index
     }
+    logger.error('The project could not be found')
+    return null
+}
+
+const findTodoIndex = (projectId, id) => {
+    const todos = findProject(projectId).todos
+    const index = todos.findIndex(todo => todo.id === id)
+    if (index !== -1) {
+        return index
+    }
+    logger.error('The todo could not be found')
     return null
 }
 
 const confirmDeletion = () => {
-    if (prompt('Are you sure? (type: "yes")') === 'yes'){
-        return true
-    }
-    return false
-}
+    return new Promise((resolve) => {
+        const userConfirmed = window.confirm('Are you sure? (click OK to confirm)');
+        if (userConfirmed) {
+            resolve(true);
+        } else {
+            logger.log('User canceled the deletion');
+            resolve(false);
+        }
+    });
+};
 
-const deleteProject = (id) => {
-    if (!confirmDeletion()) {
-        logger.log('User canceled the deletion')
-        return false
+const deleteProject = async (id) => {
+    if (!(await confirmDeletion())) {
+        return false;
     }
-    const index = findProject(id)
+    const index = findProjectIndex(id)
     if (index === null) {
-        logger.error('The project could not be found')
         return false
     }
     projects.splice(index, 1);
     return true
 }
 
-const validateProjectName = (name) => {
-    if (!name) return false
-    return true
-}
-
 const addProject = (name) => {
-    if (!validateProjectName(name)) {
-        logger.message('Name is not a valid name')
+    if (!validation.validateProjectName(name)) {
         return false
     }
     const project = {
-        id: generateId(),
-        name: name
+        id: generateProjectId(),
+        name: name,
+        todos: [],
     }
     projects.push(project)
     return true
 }
 
 const editProjectName = (id, name) => {
-    const index = findProject(id)
+    const index = findProjectIndex(id)
     if (index === null) {
-        logger.error('The project could not be found')
         return false
     }
-    if (!validateProjectName(name)) {
-        logger.message('Name is not a valid name')
+    if (!validation.validateProjectName(name)) {
         return false
     }
     projects[index].name = name
     return true
 }
 
-export {
+const deleteTodo = async (projectId, id) => {
+    if (!(await confirmDeletion())) {
+        logger.log('User canceled the deletion')
+        return false;
+    }
+    const projectIndex = findProjectIndex(projectId)
+    if (projectIndex === null) {
+        return false
+    }
+    const todoIndex = findTodoIndex(projectId, id)
+    if (todoIndex === null) {
+        return false
+    }
+    projects[projectIndex].todos.splice(todoIndex, 1);
+    return true
+}
+
+const addTodo = (projectId, title, description) => {
+    if (!validation.validateTodoTitle(title)) {
+        return false
+    }
+    if (!validation.validateTodoDescription(description)) {
+        return false
+    }
+    const projectIndex = findProjectIndex(projectId)
+    if (projectIndex === null) {
+        return false
+    }
+    projects[projectIndex].todos.push({
+        id: generateTodoId(projectId),
+        title: title,
+        description: description,
+    })
+    logger.message('Todo successfully added')
+    return true
+}
+
+const editTodo = (projectId, id, title, description) => {
+    if (!validation.validateTodoTitle(title)) {
+        return false
+    }
+    if (!validation.validateTodoDescription(description)) {
+        return false
+    }
+    const projectIndex = findProjectIndex(projectId)
+    if (projectIndex === null) {
+        return false
+    }
+    const todoIndex = findTodoIndex(projectId, id)
+    if (todoIndex === null) {
+        return false
+    }
+    projects[projectIndex].todos[todoIndex].title = title
+    projects[projectIndex].todos[todoIndex].description = description
+    return true
+}
+
+export default {
     deleteProject,
     addProject,
-    editProjectName
+    editProjectName,
+    deleteTodo,
+    addTodo,
+    editTodo,
 }
